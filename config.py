e601a5efe19e875bc26d8704a084098175e0d97e
CodeWidNSL <git config --global user.email>
2023-07-08 17:17:47 +0530

    config

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ PORT_NUMBER = 8080
 
 MODEL_FILE_PATH = os.path.join(os.getcwd(),"static/Model/medical_insurance.pkl")
 
-JSON_FILE_PATH = os.path.join(os.getcwd(),"static/JSON/Json_data.json")
+JSON_FILE_PATH = "static/JSON/Json_data.json"
